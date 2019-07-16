c45548e4ea7fd5f07496e582b910f506a0f103ee
Guilherme <guilhermebromonschenkel@gmail.com>
2019-07-15 21:19:04 -0300

    Improved connection method

--- puppeteer/services/connect.js
+++ puppeteer/services/connect.js
@@ -9,6 +9,9 @@ async function connect(page, url){
 
         console.log('Clicked on connect button...')
         await page.click('#formVideo > button')
+
+        const isConnected = await page.evaluate(() => document.querySelector('body > div.container.container-success > div > div.row.col-md-12.col-sm-12 > div > div.saudacao > div').innerText)
+        if (isConnected) return console.log('----> ALREADY CONNECTED!')
         
         console.log('Browsed into sponsor page...')
         await page.waitForSelector('#video > div.vjs-poster', { visible: true, timeout: 0 })
